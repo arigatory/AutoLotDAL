4396d640fa72ca267f0679511c82dae386469f34
Иван Иван <remillius_dies@outlook.com>
2020-01-11 02:06:34 +0300

    add adding

---
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.DataOperations
+{
+    public class InventoryDAL
+    {
+        private readonly string _connectionString;
+        public InventoryDAL() : this(@"Data Source = (localdb)\mssqllocaldb;Integrated Security=true;Initial Catalog=AutoLot")
+        {
+        }
+        public InventoryDAL(string connectionString)
+            => _connectionString = connectionString;
+
+        private SqlConnection _sqlConnection = null; private void OpenConnection()
+        {
+            _sqlConnection = new SqlConnection { ConnectionString = _connectionString };
+            _sqlConnection.Open();
+        }
+        private void CloseConnection()
+        {
+            if (_sqlConnection?.State != ConnectionState.Closed)
+            {
+                _sqlConnection?.Close();
+            }
+        }
+
+        public List<Car> GetAllInventory()
+    {
+        OpenConnection();
+        List<Car> inventory = new List<Car>();
+        string sql = "Select * From Inventory";
+        using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+        {
+            command.CommandType = CommandType.Text;
+            SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            while (dataReader.Read())
+            {
+                inventory.Add(new Car
+                {
+                    CarId = (int)dataReader["CarId"],
+                    Color = (string)dataReader["Color"],
+                    Make = (string)dataReader["Make"],
+                    PetName = (string)dataReader["PetName"]
+                });
+            }
+            dataReader.Close();
+        }
+        return inventory;
+    }
+
+
+
+}
+}
